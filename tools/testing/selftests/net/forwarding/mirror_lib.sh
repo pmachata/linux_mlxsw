7e604ba4eb8c175ac35ee94550666d0d96df7c77
Petr Machata <petrm@nvidia.com>
2024-05-27 19:44:43 +0300

    selftests: mirror libs: mirror_test(): Allow exact count of packets
    The mirroring selftests work by sending ICMP traffic between two hosts.
    Along the way, this traffic is mirrored to a gretap netdevice, and counter
    taps are then installed strategically along the path of the mirrored
    traffic to verify the mirroring took place.
    
    The problem with this is that besides mirroring the primary traffic, any
    other service traffic is mirrored as well. At the same time, because the
    tests need to work in HW-offloaded scenarios, the ability of the device to
    do arbitrary packet inspection should not be taken for granted. Most tests
    therefore simply use matchall, one uses flower to match on IP address.
    
    As a result, the selftests are noisy, because besides the primary ICMP
    traffic, any amount of other service traffic is mirrored as well.
    
    mirror_test() accommodated this noisiness by giving the counters an
    allowance of several packets. But in the previous patch, where possible,
    counter taps were changed to match only on an exact ICMP message. At least
    in those cases, we can demand an exact number of packets to match.
    
    Where the tap is installed on a connective netdevice, the exact matching is
    not practical (though with u32, anything is possible). In those places,
    there should still be some leeway -- and probably bigger than before,
    because experience shows that these tests are very noisy.
    
    To that end, change mirror_test() so that it can be either called with an
    exact number to expect, or with an expression. Where leeway is needed,
    adjust callers to pass a ">= 10" instead of mere 10.
    
    Signed-off-by: Petr Machata <petrm@nvidia.com>

--- tools/testing/selftests/net/forwarding/mirror_lib.sh
+++ tools/testing/selftests/net/forwarding/mirror_lib.sh
@@ -44,14 +44,17 @@ mirror_test()
 		local type="icmp echoreq"
 	fi
 
+	if [[ -z ${expect//[[:digit:]]/} ]]; then
+		expect="== $expect"
+	fi
+
 	local t0=$(tc_rule_stats_get $dev $pref)
 	$MZ $proto $vrf_name ${sip:+-A $sip} -B $dip -a own -b bc -q \
 	    -c 10 -d 100msec -t $type
 	sleep 0.5
 	local t1=$(tc_rule_stats_get $dev $pref)
 	local delta=$((t1 - t0))
-	# Tolerate a couple stray extra packets.
-	((expect <= delta && delta <= expect + 2))
+	((delta $expect))
 	check_err $? "Expected to capture $expect packets, got $delta."
 }
 
@@ -128,8 +131,8 @@ do_test_span_vlan_dir_ips()
 	# The traffic is meant for local box anyway, so will be trapped to
 	# kernel.
 	vlan_capture_install $dev "skip_hw vlan_id $vid vlan_ethtype $ul_proto"
-	mirror_test v$h1 $ip1 $ip2 $dev 100 $expect
-	mirror_test v$h2 $ip2 $ip1 $dev 100 $expect
+	mirror_test v$h1 $ip1 $ip2 $dev 100 "$expect"
+	mirror_test v$h2 $ip2 $ip1 $dev 100 "$expect"
 	vlan_capture_uninstall $dev
 }
 
@@ -141,7 +144,8 @@ quick_test_span_vlan_dir_ips()
 	local ip1=$1; shift
 	local ip2=$1; shift
 
-	do_test_span_vlan_dir_ips 10 "$dev" "$vid" "$ul_proto" "$ip1" "$ip2"
+	do_test_span_vlan_dir_ips '>= 10' "$dev" "$vid" "$ul_proto" \
+				  "$ip1" "$ip2"
 }
 
 fail_test_span_vlan_dir_ips()
@@ -152,7 +156,7 @@ fail_test_span_vlan_dir_ips()
 	local ip1=$1; shift
 	local ip2=$1; shift
 
-	do_test_span_vlan_dir_ips 0 "$dev" "$vid" "$ul_proto" "$ip1" "$ip2"
+	do_test_span_vlan_dir_ips '== 0' "$dev" "$vid" "$ul_proto" "$ip1" "$ip2"
 }
 
 quick_test_span_vlan_dir()
