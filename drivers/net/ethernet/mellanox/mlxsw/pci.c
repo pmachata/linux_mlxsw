31292718bfcbff976e031b007950e0256a363884
Amit Cohen <amcohen@nvidia.com>
2024-10-31 09:21:18 +0200

    mlxsw: Add support for XDP
    Call the APIs from pci_xdp.c to handle XDP program. For each Rx packet,
    check if a XDP program is set for the Rx netdevice, if so, initialize
    XDP buffer and run XDP program. Handle the packet after running XDP
    according to XDP return status.
    
    Note that there is no need to use RCU lock to access XDP program pointer,
    as this code runs from NAPI poll method, which means that bottom half is
    disabled, so we can access RCU pointer via rcu_dereference_bh() without an
    additional lock.
    
    Using XDP drop action significantly improves performance. Packet drop rate
    of mlxsw driver increases by 3.5x compared to dropping packets with TC.
    
    Signed-off-by: Amit Cohen <amcohen@nvidia.com>
    Reviewed-by: Ido Schimmel <idosch@nvidia.com>
    Signed-off-by: Ido Schimmel <idosch@nvidia.com>

--- drivers/net/ethernet/mellanox/mlxsw/pci.c
+++ drivers/net/ethernet/mellanox/mlxsw/pci.c
@@ -23,6 +23,7 @@
 #include "port.h"
 #include "resources.h"
 #include "txheader.h"
+#include "pci_xdp.h"
 
 #define mlxsw_pci_write32(mlxsw_pci, reg, val) \
 	iowrite32be(val, (mlxsw_pci)->hw_addr + (MLXSW_PCI_ ## reg))
@@ -803,6 +804,40 @@ static void mlxsw_pci_cqe_rdq_md_init(struct sk_buff *skb, const char *cqe)
 	mlxsw_pci_cqe_rdq_md_tx_port_init(skb, cqe);
 }
 
+/* Returns true if XDP handled the packet, and false if networking stack should
+ * handle it.
+ */
+static bool
+mlxsw_pci_xdp_handle(struct mlxsw_pci *mlxsw_pci, struct mlxsw_pci_queue *q,
+		     struct xdp_buff *xdp_buff,
+		     struct mlxsw_pci_rx_pkt_info *rx_pkt_info,
+		     u16 local_port)
+{
+	struct skb_shared_info *skb_shared_info;
+	struct mlxsw_pci_xdp_port *xdp_port;
+	enum mlxsw_xdp_status xdp_status;
+	struct bpf_prog *prog;
+
+	xdp_port = &mlxsw_pci->xdp_ports[local_port];
+	prog = rcu_dereference_bh(xdp_port->xdp_prog);
+	if (!prog)
+		return false;
+
+	mlxsw_xdp_buff_init(xdp_buff, rx_pkt_info, &q->u.rdq.xdp_rxq);
+
+	xdp_status = mlxsw_xdp_run(xdp_buff, prog, xdp_port->netdev);
+	switch (xdp_status) {
+	case MLXSW_XDP_STATUS_DROP:
+	case MLXSW_XDP_STATUS_FAIL:
+		skb_shared_info = xdp_get_shared_info_from_buff(xdp_buff);
+		mlxsw_pci_rdq_pages_recycle(q, rx_pkt_info->pages,
+					    skb_shared_info->nr_frags + 1);
+		return true;
+	}
+
+	return true;
+}
+
 static void mlxsw_pci_cqe_rdq_handle(struct mlxsw_pci *mlxsw_pci,
 				     struct mlxsw_pci_queue *q,
 				     u16 consumer_counter_limit,
@@ -812,6 +847,7 @@ static void mlxsw_pci_cqe_rdq_handle(struct mlxsw_pci *mlxsw_pci,
 	struct pci_dev *pdev = mlxsw_pci->pdev;
 	struct mlxsw_pci_queue_elem_info *elem_info;
 	struct mlxsw_rx_info rx_info = {};
+	struct xdp_buff xdp_buff = {};
 	struct sk_buff *skb;
 	u16 byte_count;
 	int err;
@@ -850,6 +886,10 @@ static void mlxsw_pci_cqe_rdq_handle(struct mlxsw_pci *mlxsw_pci,
 	err = mlxsw_pci_rdq_pages_alloc(q, elem_info,
 					rx_pkt_info.num_sg_entries);
 	if (err)
+		goto out;
+
+	if (mlxsw_pci_xdp_handle(mlxsw_pci, q, &xdp_buff, &rx_pkt_info,
+				 rx_info.local_port))
 		goto out;
 
 	skb = mlxsw_pci_rdq_build_skb(q, &rx_pkt_info);
